c7e8bdc81914fb16f138b809f6fb3975540745f7
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-05-12 10:47:48 +0100

    use two spaces indentation

--- app/test.component.ts
+++ app/test.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import {HTTP_PROVIDERS} from '@angular/http';
 
 declare var __moduleName: string;
 @Component({
-    moduleId: __moduleName,
-    selector: 'my-test-component',
-    template: `<h1>Hello Angular2</h1>`,
-    providers: [
-        ROUTER_PROVIDERS, HTTP_PROVIDERS
-    ]
+  moduleId: __moduleName,
+  selector: 'my-test-component',
+  template: `<h1>Hello Angular2</h1>`,
+  providers: [
+    ROUTER_PROVIDERS, HTTP_PROVIDERS
+  ]
 })
 export class TestComponent implements OnInit {
-    constructor() { }
+  constructor() { }
 
-    ngOnInit() { }
+  ngOnInit() { }
 }
