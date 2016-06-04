4433dc134d936746dc40d2d410bf9bff28d40f45
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-06-04 22:54:41 +0100

    upgrade packages use shims from core.js add ngrx/store state management library

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,45 +1,70 @@
+/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ */
 (function (global) {
+  // map tells the System loader where to look for things
+  var map = {
+    'app': 'app', // 'dist',
+    '@angular': 'node_modules/@angular',
+    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+    'rxjs': 'node_modules/rxjs',
+    'ng2-bootstrap': 'node_modules/ng2-bootstrap/ng2-bootstrap',
+    'moment': 'node_modules/moment/moment.js',
+    
+    // https://github.com/ngrx/devtools
+    //'@ngrx/devtools': 'node_modules/@ngrx/devtools',
+    
+    // https://github.com/ngrx/store
+    //'@ngrx/core': 'node_modules/@ngrx/core',
+    //'@ngrx/store': 'node_modules/@ngrx/store',
+    
+    '@ngrx': 'node_modules/@ngrx'
+  };
 
-    // map tells the System loader where to look for things
-    var map = {
-        'app': 'app', // 'dist',
-        'rxjs': 'node_modules/rxjs',
-        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-        '@angular': 'node_modules/@angular'
-    };
+  // packages tells the System loader how to load when no filename and/or no extension
+  var packages = {
+    'app': { main: 'main.js', defaultExtension: 'js' },
+    'rxjs': { defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+    'node_modules/ng2-bootstrap': { defaultExtension: 'js' },
+    '@ngrx/core': { main: 'index.js', defaultExtension: 'js' },
+    '@ngrx/store': { main: 'index.js', defaultExtension: 'js' },
+    //'@ngrx/devtools': { main: 'dist/index.js', defaultExtension: 'js' }
+  };
 
-    // packages tells the System loader how to load when no filename and/or no extension
-    var packages = {
-        'app': { main: 'main.js', defaultExtension: 'js' },
-        'rxjs': { defaultExtension: 'js' }
-    };
+  var ngPackageNames = [
+    'common',
+    'compiler',
+    'core',
+    'http',
+    'platform-browser',
+    'platform-browser-dynamic',
+    'router',
+    'router-deprecated',
+    'upgrade',
+  ];
 
-    var packageNames = [
-        '@angular/common',
-        '@angular/compiler',
-        '@angular/core',
-        '@angular/http',
-        '@angular/platform-browser',
-        '@angular/platform-browser-dynamic',
-        '@angular/router',
-        '@angular/router-deprecated',
-        '@angular/testing',
-        '@angular/upgrade'
-    ];
+  // Individual files (~300 requests):
+  function packIndex(pkgName) {
+    packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  }
 
-    // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
-    packageNames.forEach(function (pkgName) {
-        packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
-    });
+  // Bundled (~40 requests):
+  function packUmd(pkgName) {
+    packages['@angular/' + pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
+  };
 
-    var config = {
-        map: map,
-        packages: packages
-    }
+  // Most environments should use UMD; some (Karma) need the individual index files
+  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
 
-    // filterSystemConfig - index.html's chance to modify config before we register it.
-    if (global.filterSystemConfig) { global.filterSystemConfig(config); }
+  // Add package entries for angular packages
+  ngPackageNames.forEach(setPackageConfig);
 
-    System.config(config);
+  var config = {
+    map: map,
+    packages: packages
+  }
 
+  System.config(config);
 })(this);
