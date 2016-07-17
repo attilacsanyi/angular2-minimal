5b5ca53728921d3e9bd05af768b8b3fc8ac162b7
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-07-17 10:42:43 +0100

    replace system module format to commons

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,11 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import {HTTP_PROVIDERS} from '@angular/http';
 
-declare var __moduleName: string;
 @Component({
-  moduleId: __moduleName,
   selector: 'my-app-component',
-  templateUrl: 'app.component.html',
+  templateUrl: 'app/app.component.html',
   providers: [
     ROUTER_PROVIDERS, HTTP_PROVIDERS
   ]
--- systemjs.config.js
+++ systemjs.config.js
@@ -52,8 +52,8 @@
 
   // Bundled (~40 requests):
   function packUmd(pkgName) {
-    packages['@angular/' + pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
-  };
+    packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+  }
 
   // Most environments should use UMD; some (Karma) need the individual index files
   var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
@@ -61,10 +61,13 @@
   // Add package entries for angular packages
   ngPackageNames.forEach(setPackageConfig);
 
+  // No umd for router yet
+  packages['@angular/router'] = { main: 'index.js', defaultExtension: 'js' };
+
   var config = {
     map: map,
     packages: packages
-  }
+  };
 
   System.config(config);
 })(this);
